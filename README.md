4b548ca9c7c8fa4c7146fedd7045e15a8ae9b679
benjamin <benjaminverlaine@gmail.com>
2024-05-23 13:53:59 +0200

    Configuration de Vite pour le multi pages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mon-blog
+
+## projet numéro 2 de la formation javascript de Dyma.fr
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./src",
     build: {
         outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, "src/index.html"),
+                form: resolve(__dirname, "src/form/form.html"),
+            },
+        },
     },
 });
